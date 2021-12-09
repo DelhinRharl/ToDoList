77c52b0539a4cc4e1c2fc9234ca08c00d8ba9cf3
Affaxed <delhinrharl@gmail.com>
2021-12-09 17:11:50 +0000

    Add interactive functionality to page

--- src/index.js
+++ src/index.js
@@ -1,36 +1,9 @@
 import './style.css';
+import {
+  showStorage, ifChecked, storeList, addList,
+} from './check.js';
 
-const myList = [
-  {
-    description: 'Wash the dishes',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Wash the dishes',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Clean the house',
-    completed: false,
-    index: 2,
-  },
-];
-
-const list = document.getElementById('licontainer');
-
-// add items to dom
-const addList = (array, element) => {
-  array.forEach((item) => {
-    const listItems = document.createElement('li');
-    listItems.classList.add('mystyle');
-    element.appendChild(listItems);
-    listItems.innerHTML = `
-    <input type="checkbox" class = "check" id="${item.index}">
-    <label for="${item.index}">${item.description}&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-    <i class="fas fa-ellipsis-v"></i>
-    `;
-  });
-};
-addList(myList, list);
+addList();
+storeList();
+ifChecked();
+showStorage();
